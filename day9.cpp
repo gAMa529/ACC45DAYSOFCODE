ba42ed2e4b4e0042e5ba77b4ff3cd894f5554372
HAPPY Happy <randeeps2000@gmil.com>
2022-10-31 23:09:00 +0530

    day9

---
+++ day9.cpp
@@ -0,0 +1,17 @@
+// codechef problem code SHOPCHANGE
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    do
+    {
+        int x;
+        cin >> x;
+        cout << 100 - x << endl;
+        t--;
+    } while (t != 0);
+    return 0;
+}
